99cc0a26172229e262b8c85141dd55cdc243e4ad
Fatima Aamir <108588610+fatima-aamir@users.noreply.github.com>
2023-09-19 21:27:16 +0500

    Fatima added the subtract

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,9 @@
 from func1 import multiply
 from func1 import divide
+from Func3 import sub
 
+def test_subtract():
+    assert 4 == sub(6,2)
+    
 def test_multiply():
     assert 6 == multiply(3,2)
